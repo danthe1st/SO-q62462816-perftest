2244007b18d1f6d976a360f44acba87f341c3b28
danthe1st <daniel@wwwmaster.at>
2020-06-19 20:12:18 +0200

    initial commit

---
+++ TesteMatrixMultiply.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+public class TesteMatrixMultiply{
+
+    public final static int n  =  1024;
+    static double[][] A = new double[n][n];
+    static double[][] B = new double[n][n];
+    static double[][] C = new double[n][n];
+
+    public static void main (String args[])
+    {
+        Random r = new Random();
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                A[i][j] = r.nextDouble();
+                B[i][j] = r.nextDouble();
+                C[i][j] = 0;
+            }
+        }
+
+        long start = System.nanoTime();
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                for (int k = 0; k < n; ++k)
+                {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        long stop = System.nanoTime();
+        double tdiff = (stop - start) * 1e-9;
+        System.out.println(tdiff);
+
+        double d = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                d+= C[i][j];
+            }
+        }
+
+        System.out.printf("Fim %.6f\n", d);
+
+    }
+
+}
